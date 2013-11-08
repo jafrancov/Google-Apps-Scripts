ef1bc2587fba0697a4cc8b00e4937f52943dd6c1
Alex Franco <jafrancov@gmail.com>
2013-11-07 21:56:58 -0600

    Email on form submit
    Script to send an email after someone submit a Google Drive Form

---
+++ EmailOnFormSubmit.js
@@ -0,0 +1,34 @@
+function emailOnFormSubmit(e) {
+
+	// Create as many variables as answers (columns in your spreadsheet) you require to send
+	var timestamp = e.values[0];
+	var name = e.values[1];
+	var city = e.values[2];
+	var mail = e.values[3];
+	
+	// The subject of the email
+	var subject = "Test Confirmation for " + name;
+
+	// emailBody is for those devices that can't render HTML, is plain text
+	var emailBody = "This is an email test for the user " + Name + 
+					"\nFrom " + City + 
+					"\nWith email " + Mail + 
+					"\nRegister on " + Timestamp
+					"\n\nThank you for register!"; 
+	
+	// html is for those devices that can render HTML
+	// nowadays almost all devices can render HTML
+	var htmlBody =  "Thank you, your form was submitted on <i>" + timestamp + "</i>" + 
+					"<br/><br/>The details you entered were as follows: " +
+					"<br/>Your Name: <font color=\"red\"><strong>" + name + "</strong></font>" +
+					"<br/>From: " + city + 
+					"<br/>With email: " + mail;
+	
+	// More info for Advanced Options Parameters 
+	// https://developers.google.com/apps-script/reference/mail/mail-app#sendEmail(String,String,String,Object)
+	var advancedOpts = { name: "Forms - Alex Franco", htmlBody: htmlBody };
+
+	// This instruction sends the email
+	MailApp.sendEmail(mail, subject, emailBody, advancedOpts);
+	
+}
