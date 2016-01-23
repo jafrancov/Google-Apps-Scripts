22ea05de28ea99868d4d7417ee3f85d6e77367f7
Alex Franco <jafrancov@gmail.com>
2016-01-23 17:45:37 -0600

    Sequence number on form submit

---
+++ forms/sequenceOnFormSubmit.js
@@ -0,0 +1,36 @@
+function sequenceNumberOnFormSubmit(e) {
+    record = addSequenceNumber();
+
+	var timestamp = e.values[0];
+	var name = e.values[1];
+	var mail = e.values[2];
+	
+	var subject = "Request number " + record;
+
+	var plain_email_body = "Hello " + name + '!' +
+					"\n\n" + 
+					"We have registered you request, sent on " + timestamp +
+					"\n\n" + 
+					"To follow you status ask for the request number" + record + 
+	var html_body =  "Hello " + name + '!' + 
+					"<br/><br/>" + 
+					"We have registered you request, sent on <i>" + timestamp + "</i>" +
+					"<br/><br/>" + 
+					"To follow the status ask for the request number <font color=\"red\"><strong>" + record + "</strong></font>";
+
+	var advancedOpts = { name: "Forms - Alex Franco", htmlBody: html_body };
+	MailApp.sendEmail(mail, subject, plain_email_body, advancedOpts);
+}
+
+function addSequenceNumber() {
+	// Obtain the sheet where we save the answers
+	var sheet = SpreadsheetApp.getActiveSheet();
+	// Obtain the last row with data
+	var row =  SpreadsheetApp.getActiveSheet().getLastRow();
+	// Sequence number (record) minus 1, this is due to the headers
+	record = row - 1
+	// Set (or write) the sequence number in the cell specified
+	sheet.getRange(row,4).setValue(record);
+	// Return the sequence number
+	return record;
+}
